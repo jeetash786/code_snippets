a9294882df4dfcd0efe2003d425aedb80a5eb499
jeetash786 <136995941+jeetash786@users.noreply.github.com>
2024-06-10 18:33:06 +0500

    Update oop.py
    Hey! When I checked the code of 2nd video of OOPs, The code wasn't same in the video.

--- Object-Oriented/2-Class-Instance-Variables/oop.py
+++ Object-Oriented/2-Class-Instance-Variables/oop.py
@@ -1,14 +1,33 @@
+#OLD AND SAME CODE OF PREVIOUS LECTURE
+# class Employee:
+
+#     def __init__(self, first, last, pay):
+#         self.first = first
+#         self.last = last
+#         self.email = first + '.' + last + '@email.com'
+#         self.pay = pay
+
+#     def fullname(self):
+#         return '{} {}'.format(self.first, self.last)
+
+# emp_1 = Employee('Corey', 'Schafer', 50000)
+# emp_2 = Employee('Test', 'Employee', 60000)
+
+#SIMPLE AND CODE OF 2nd LECTURE
 
 class Employee:
-
-    def __init__(self, first, last, pay):
+    raise_amount = 1.04
+    no_emp = 0
+    def __init__(self,first,last,pay):
         self.first = first
         self.last = last
-        self.email = first + '.' + last + '@email.com'
         self.pay = pay
-
-    def fullname(self):
-        return '{} {}'.format(self.first, self.last)
-
-emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('Test', 'Employee', 60000)
+        Employee.no_emp += 1
+    
+    def pay_raise(self):
+        self.pay = int(self.pay *  self.raise_amount)
+emp_1 = Employee('PEWDIE','PIE',5000)
+emp_2 = Employee('Mr','Beast',5000)
+emp_1.pay_raise()
+print(emp_1.pay)
+print(Employee.no_emp)
